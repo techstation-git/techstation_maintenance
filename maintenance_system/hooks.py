ff4ac8ce7c57109008a5a6d000c3b29cb0ef8e5b
EmperorTatty <abedtati1@gmail.com>
2026-03-04 13:23:24 +0300

    feat: add Maintenance Operations Dashboard as Custom HTML Block fixture

--- maintenance_system/hooks.py
+++ maintenance_system/hooks.py
@@ -3,6 +3,14 @@ app_title = 'maintenance_system'
 app_description = 'maintenance_system description'
 app_version = '0.0.1'
 
+# Fixtures to export/import with bench migrate
+fixtures = [
+    {
+        "dt": "Custom HTML Block",
+        "filters": [["name", "=", "Maintenance Operations Dashboard"]]
+    }
+]
+
 # Document event hooks for notifications
 doc_events = {
     "Maintenance Order": {
